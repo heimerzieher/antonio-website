2ac74d0da3ac8895251315f4c47aa61bf897452c
0xFEEDC0DE64 <daniel@brunner.ninja>
2022-05-16 17:11:30 +0200

    Workflow tests

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <?php include 'navbar.php'; ?>
+
+        <h1>Hello World</h1>
+
+        <p>This is the startpage.</p>
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,9 @@
+        <nav>
+            <ul>
+                <li><a href="/index.html">Startpage</a></li>
+                <li><a href="/page-a.html">Page A</a></li>
+                <li><a href="/page-B.html">Page B</a></li>
+                <li><a href="/page-C.html">Page C</a></li>
+                <li><a href="/page-D.html">Page D</a></li>
+            </ul>
+        </nav>

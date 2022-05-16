edcccf27bab3beeb35610dab26d7558f0e2f0b68
0xFEEDC0DE64 <daniel@brunner.ninja>
2022-05-16 18:10:20 +0200

    Example page layout

--- index.php
+++ index.php
@@ -1,14 +1,103 @@
-<html>
+<!doctype html>
+<html lang="en">
     <head>
-        <title>Hello World</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Bootstrap demo</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
     </head>
     <body>
-<?php include 'navbar.php'; ?>
+        <div class="container" id="mainContainer">
+            <small>Name Castame</small>
 
-        <h1>Hello World</h1>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">CV</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">TEACHING</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">AOI</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">EMAIL</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">SOCIAL</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
-        <p>This is the startpage.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu dictum risus. Donec viverra porttitor nisl ut sodales. Sed et sollicitudin justo, sed ultrices augue. Proin sed posuere mi, sed dictum justo. Integer eget augue sed massa elementum interdum. Duis non nisl suscipit purus rutrum viverra. Nullam venenatis dapibus sapien et aliquet. Nulla sagittis urna pharetra dolor mollis, vitae lacinia tellus auctor. In rutrum lorem non semper suscipit. In eget lorem nisl. Praesent leo nulla, feugiat ac finibus ac, iaculis nec neque. Aenean nec auctor eros, commodo luctus massa. Aenean sit amet nisl et velit sagittis aliquam maximus nec eros. Sed sagittis libero sit amet tortor tristique fringilla. Nam non mauris lobortis, sagittis mauris id, scelerisque est.</p>
+            <p>Nulla tempor, augue vel euismod vehicula, lorem quam maximus leo, at finibus eros ex quis massa. Vivamus posuere orci ornare, commodo orci sit amet, commodo sapien. Donec arcu felis, elementum tempor congue ac, rhoncus ac urna. Phasellus nibh leo, interdum eget laoreet quis, ullamcorper nec ipsum. Fusce maximus non leo eget vulputate. Quisque viverra in lectus sodales blandit. Nulla et massa vitae magna porttitor malesuada. Mauris maximus lacus ac arcu porta, vitae sodales arcu finibus. Vivamus nec magna vel ante vestibulum vulputate id ac turpis.</p>
+            <p>In malesuada, massa vel imperdiet venenatis, augue neque finibus nibh, sed facilisis lectus justo eget arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras luctus lacinia mi at viverra. Quisque sodales fermentum enim vitae consequat. Integer lorem justo, luctus facilisis imperdiet vel, ultricies et purus. Ut vel efficitur odio. In placerat mi a lacus molestie mattis. Morbi sagittis a tortor ac mollis. Maecenas facilisis bibendum est, pretium feugiat ante cursus eget. Nullam placerat tincidunt convallis. Sed dapibus massa sed ipsum gravida blandit. Maecenas eget cursus velit. Quisque semper eu nisi in malesuada. Pellentesque vitae rutrum odio.</p>
+            <p>Vestibulum posuere lectus dui, ut pretium purus rutrum et. Sed in augue ut lacus vulputate tristique. Aliquam vel magna congue, auctor ex sed, fringilla turpis. Nam hendrerit ultricies enim, ac placerat ligula pellentesque a. Suspendisse quis interdum sem. Nam ultricies diam sed condimentum fermentum. Ut libero dui, lobortis vitae dui sed, consequat efficitur quam. Suspendisse potenti. Aliquam et semper tortor.</p>
+            <p>Phasellus lacinia dui vitae rutrum ultricies. In rutrum leo et dui venenatis congue. Aenean pretium fringilla porttitor. Donec eget blandit arcu. Aliquam commodo mauris eget neque viverra, sodales facilisis tellus bibendum. Pellentesque ultrices ante libero, eu mattis massa varius non. Phasellus a est elementum, rhoncus arcu hendrerit, dictum diam. Nulla nec ligula quis erat vulputate egestas. Proin at risus at leo imperdiet mollis. Morbi ex sapien, malesuada id ullamcorper in, venenatis et nunc. Nulla vulputate urna ultrices quam laoreet malesuada. Etiam ultrices nulla sed molestie aliquam. Vestibulum eleifend et neque eu vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et finibus lectus.</p>
 
-<?php include 'footer.php'; ?>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">PROJECTS</th>
+                        <th scope="col">JOURNAL / PRESS</th>
+                        <th scope="col">YEAR</th>
+                        <th scope="col">CO-AUTHORS</th>
+                        <th scope="col">CATEGORY</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">A</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">B</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">C</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">D</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">E</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </body>
 </html>
